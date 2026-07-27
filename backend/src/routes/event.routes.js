33cfcf2c01432920f80d0e24252982f46e74ff81
tanzy211 <neotanzy211@outlook.com>
2026-07-28 01:44:48 +1200

    Implement event CRUD API

--- backend/src/routes/event.routes.js
+++ backend/src/routes/event.routes.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    getAllEvents
+    getAllEvents,
+    getEventById,
+    createEvent,
+    updateEventById,
+    deleteEventById
+    
 } = require('../controllers/event.controller');
 
 router.get('/', getAllEvents);
+
+router.get('/:id', getEventById);
+
+router.post('/', createEvent);
+
+router.put('/:id', updateEventById);
+
+router.delete('/:id', deleteEventById);
 
 module.exports = router;
